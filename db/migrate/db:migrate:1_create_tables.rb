bb301dd2377a8956fd5f6435b7d9f7c9a38115ee
DeeHayer <Mon@Mons-MacBook-Air.local>
2015-02-23 20:10:17 -0800

    migrate create tables

---
+++ db/migrate/db:migrate:1_create_tables.rb
@@ -0,0 +1,25 @@
+class CreateTables < ActiveRecord::Migration
+
+    def change
+        create_table :users do |t|
+            t.string :username
+            t.string :email
+            t.string :password
+            t.timestamps
+        end
+
+        create_table :style do |t|
+            t.string :bridal_lehenga
+            t.string :lehenga
+            t.string :sari 
+            t.string :salwar_kameez
+            t.timestamps
+        end
+
+        create_table :pins
+            t.string :price
+            t.string :description
+
+    end
+
+end
